cb1ac2a57d8e0ab95627ff7c3f76c1561a81f5b3
Anmesh Mishra <108453134+MrAnmesh@users.noreply.github.com>
2023-09-26 21:53:10 +0530

    Add files via upload

---
+++ Array/DeclarationOfArray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Different ways to declare an array:-
+	
+	int arr[];
+	int brr[10];
+	int crr[] = {1,4,6,8};
+	int crr[5] = {1,2,3,4,5};
+}
